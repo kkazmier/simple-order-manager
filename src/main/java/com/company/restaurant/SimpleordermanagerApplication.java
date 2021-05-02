e86f8045215aa02ee395844f8b1f59a8771ce066
kkazm <s14005@pjwstk.edu.pl>
2021-05-02 18:58:03 +0200

    Initial commit.

---
+++ src/main/java/com/company/restaurant/SimpleordermanagerApplication.java
@@ -0,0 +1,13 @@
+package com.company.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleordermanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleordermanagerApplication.class, args);
+    }
+
+}
