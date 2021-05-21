956c896f7ad1acaf606bd81990d7f8ff7c4f2446
kkazm <s14005@pjwstk.edu.pl>
2021-05-21 17:26:10 +0200

    Change code.

--- src/main/java/com/company/restaurant/controller/OrderController.java
+++ src/main/java/com/company/restaurant/controller/OrderController.java
@@ -4,19 +4,41 @@ import com.company.restaurant.domain.Order;
 import com.company.restaurant.service.OrderServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/order/")
 @AllArgsConstructor
+@Controller
 public class OrderController {
     private final OrderServiceImpl orderService;
 
-    @GetMapping("all")
-    List<Order> getAllOrders(){
+    @ModelAttribute("ordersList")
+    public List<Order> getOrdersList(){
         return orderService.getAllOrders();
+    }
+
+    @GetMapping({ "/", "/index" })
+    public String main(Model model){
+        model.addAttribute("name", "John");
+        return "index";
+    }
+
+    @GetMapping("all")
+    public List<Order> getAllOrders(){
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("orders")
+    public String orders(Model model){
+        model.addAttribute("orders", orderService.getAllOrders());
+        System.out.println(orderService.getAllOrders().size());
+        return "orders.html";
     }
 
     @PostMapping(value = "create", consumes = MediaType.APPLICATION_JSON_VALUE)
