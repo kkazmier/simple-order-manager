78aaabf61cb0f55a0af2dcbb8d62bf626c81d9a3
kkazm <s14005@pjwstk.edu.pl>
2021-05-21 17:27:49 +0200

    Change path.

--- src/main/java/com/company/restaurant/config/WebMVCConfig.java
+++ src/main/java/com/company/restaurant/config/WebMVCConfig.java
@@ -41,7 +41,7 @@ public class WebMVCConfig implements WebMvcConfigurer, ApplicationContextAware {
         // resource resolution infrastructure, which is highly recommended.
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setApplicationContext(this.applicationContext);
-        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setPrefix("/templates/");
         templateResolver.setSuffix(".html");
         // HTML is the default value, added here for the sake of clarity.
         templateResolver.setTemplateMode(TemplateMode.HTML);
