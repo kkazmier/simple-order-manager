d1405759c997fafd0b8122e023283e106b3c2a26
kkazm <s14005@pjwstk.edu.pl>
2021-06-04 13:59:29 +0200

    Add two fields.

--- src/main/java/com/company/restaurant/domain/Order.java
+++ src/main/java/com/company/restaurant/domain/Order.java
@@ -10,10 +10,17 @@ import javax.persistence.*;
 @Getter
 @Setter
 public class Order extends BaseEntity{
+
     @ManyToOne
     @JoinColumn(name="driver_id", referencedColumnName = "id")
     private Driver driver;
 
+    @Column(name = "adress")
+    private String address;
+
     @Column(name = "food_type")
     private String foodType;
+
+    @Column(name = "status")
+    private String status;
 }
