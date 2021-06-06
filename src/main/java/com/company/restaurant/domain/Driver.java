7d77c72285538a69b2fea060bc12b8391086789a
kkazm <s14005@pjwstk.edu.pl>
2021-06-06 15:04:32 +0200

    Add Lombok annotations.

--- src/main/java/com/company/restaurant/domain/Driver.java
+++ src/main/java/com/company/restaurant/domain/Driver.java
@@ -1,5 +1,8 @@
 package com.company.restaurant.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -7,6 +10,8 @@ import java.util.Set;
 
 @Entity
 @Table(name = "drivers")
+@Getter
+@Setter
 public class Driver extends Employee{
     @OneToMany(mappedBy="driver")
     private Set<Order> orders;
