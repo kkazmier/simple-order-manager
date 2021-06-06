cab304cee85634dc54313bf1040da83b26adf8c7
kkazm <s14005@pjwstk.edu.pl>
2021-06-06 15:05:42 +0200

    Add Lombok annotations.

--- src/main/java/com/company/restaurant/domain/Employee.java
+++ src/main/java/com/company/restaurant/domain/Employee.java
@@ -1,9 +1,14 @@
 package com.company.restaurant.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@Getter
+@Setter
 public class Employee extends BaseEntity{
     @Column(name = "first_name")
     private String firstName;
