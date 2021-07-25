667f614eb90c04c74d182dd5d07ca77a5e19a05c
kkazm <s14005@pjwstk.edu.pl>
2021-07-25 14:38:01 +0200

    Add allOrderPoints, newOrderPoints, assignedOrderPoints model attributes.

--- src/main/java/com/company/restaurant/controller/StreetMapController.java
+++ src/main/java/com/company/restaurant/controller/StreetMapController.java
@@ -1,7 +1,11 @@
 package com.company.restaurant.controller;
 
 import com.company.restaurant.domain.Address;
+import com.company.restaurant.domain.OrderPoint;
+import com.company.restaurant.domain.OrderStatus;
 import com.company.restaurant.service.AddressServiceImpl;
+import com.company.restaurant.service.OrderPointServiceImpl;
+import com.company.restaurant.service.OrderServiceImpl;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
@@ -18,35 +23,76 @@ import java.util.List;
 public class StreetMapController {
     private final Logger logger = LoggerFactory.getLogger(StreetMapController.class);
     private final AddressServiceImpl addressService;
+    private final OrderServiceImpl orderService;
+    private final OrderPointServiceImpl orderPointService;
 
-    @ModelAttribute("allAddressesArray")
-    public Address[] getAddressesArray() throws Exception {
-        List<Address> addresses = addressService.findAll();
-        Address[] addressesArray = new Address[1];
-        addressesArray[0] = addresses.get(0);
-        addressService.setGeographicalCoords(addresses.get(0).getId());
-        logger.info(addressesArray[0].toString());
-//        Address[] addressesArray = new Address[addresses.size()];
-//        addresses.stream().forEach(address -> {
-//            try {
-//                addressService.setGeographicalCoords(address.getId());
-//                logger.info(address.toString());
-//                for(int i=0; i<addresses.size(); i++){
-//                    addressesArray[i] = addresses.get(i);
-//
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        });
-        return addressesArray;
+    @ModelAttribute("allOrderPoints")
+    public List<OrderPoint> getAllOrderPoints(){
+        List<OrderPoint> orderPoints = orderPointService.getAllOrderPoints();
+        logger.info("Get order points: " + orderPoints.size());
+        return orderPoints;
     }
+
+    @ModelAttribute("newOrderPoints")
+    public List<OrderPoint> getNewOrderPoints(){
+        List<OrderPoint> orderPoints = new ArrayList<>();
+        orderPointService.getAllOrderPoints().stream().forEach(
+                orderPoint -> {
+                    if (orderPoint.getStatus().equals(OrderStatus.NEW)){
+                        orderPoints.add(orderPoint);
+                    }
+                }
+        );
+        return orderPoints;
+    }
+
+    @ModelAttribute("assignedOrderPoints")
+    public List<OrderPoint> getAssignedOrderPoints(){
+        List<OrderPoint> orderPoints = new ArrayList<>();
+        orderPointService.getAllOrderPoints().stream().forEach(
+                orderPoint -> {
+                    if (orderPoint.getStatus().equals(OrderStatus.ASSIGNED)){
+                        orderPoints.add(orderPoint);
+                    }
+                }
+        );
+        return orderPoints;
+    }
+
+    @ModelAttribute("completedOrderPoints")
+    public List<OrderPoint> getCompletedOrderPoints(){
+        List<OrderPoint> orderPoints = new ArrayList<>();
+        orderPointService.getAllOrderPoints().stream().forEach(
+                orderPoint -> {
+                    if (orderPoint.getStatus().equals(OrderStatus.COMPLETED)){
+                        orderPoints.add(orderPoint);
+                    }
+                }
+        );
+        logger.info("Get order points: " + orderPoints.size());
+        return orderPoints;
+    }
+
+    @ModelAttribute("allAddresses")
+    public List<Address> getAllAddresses() {
+        List<Address> addresses = addressService.findAll();
+        addresses.stream().forEach(address -> {
+            if(address.getLongitude() == null || address.getLatitude() == null){
+                try {
+                    addressService.setGeographicalCoords(address.getId());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                logger.info(address.toString());
+            }
+        });
+        return addresses;
+    }
+
+
 
     @GetMapping("map")
     public String showMap(){
         return "streetMap.html";
     }
-
-
-
 }
