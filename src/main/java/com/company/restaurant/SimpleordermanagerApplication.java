40de8c678d97929ca16c44d7036621037e1e2e2d
kkazm <s14005@pjwstk.edu.pl>
2021-08-03 15:42:54 +0200

    Fix display main page problem.

--- src/main/java/com/company/restaurant/SimpleordermanagerApplication.java
+++ src/main/java/com/company/restaurant/SimpleordermanagerApplication.java
@@ -2,9 +2,16 @@ package com.company.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class SimpleordermanagerApplication {
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleordermanagerApplication.class, args);
